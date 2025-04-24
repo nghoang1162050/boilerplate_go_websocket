a8d102474a7acf7c91a342f12f918ff77077e5e7
hoangn <1162050@gmail.com>
2025-04-24 11:34:38 +0700

    chore: gormgen entity and functions related

--- cmd/gentool.go
+++ cmd/gentool.go
@@ -17,25 +17,59 @@ func GenModels() {
     }
 
 	g := gen.NewGenerator(gen.Config{
-		ModelPkgPath: "internal/model",
+		ModelPkgPath: "./model",
+		OutPath: 	"./internal/gorm_gen",
+		OutFile:	"gentool.go",
 		Mode: gen.WithDefaultQuery | gen.WithQueryInterface,
     })
 
 	g.UseDB(db)
 	users := g.GenerateModel("users")
 
+	// Define the relationship between rooms and users
 	room_options := field.RelateConfig{
 		GORMTag: field.GormTag{
 			"foreignKey": []string{"room_user"},
-			"references": []string{"HostID"},
+			"references": []string{"id"},
 		},
 	}
-	rooms := g.GenerateModel("rooms", gen.FieldRelate(field.HasOne, "Users", users, &room_options))
+	rooms := g.GenerateModel("rooms", gen.FieldRelate(field.HasOne, "Host", users, &room_options))
 
-	// messages := g.GenerateModel("messages")
-	// room_members := g.GenerateModel("room_members")
+	// Define the relationship between rooms and users, room details.
+	room_members_user_options := field.RelateConfig{
+		GORMTag: field.GormTag{
+			"foreignKey": []string{"room_members_user"},
+			"references": []string{"id"},
+		},
+	}
+	room_members_room_options := field.RelateConfig{
+		GORMTag: field.GormTag{
+			"foreignKey": []string{"room_members_room"},
+			"references": []string{"id"},
+		},
+	}
+	room_members := g.GenerateModel("room_members", 
+		gen.FieldRelate(field.HasMany, "RoomMembers", users, &room_members_user_options),
+		gen.FieldRelate(field.HasMany, "Room", users, &room_members_room_options))
 
-	g.ApplyBasic(users, rooms)
+	// Define the relationship between messages and users, room details.
+	messages_room_options := field.RelateConfig{
+		GORMTag: field.GormTag{
+			"foreignKey": []string{"messages_room"},
+			"references": []string{"id"},
+		},
+	}
+	messages_user_options := field.RelateConfig{
+		GORMTag: field.GormTag{
+			"foreignKey": []string{"messages_user"},
+			"references": []string{"id"},
+		},
+	}
+	messages := g.GenerateModel("messages",
+		gen.FieldRelate(field.HasOne, "Room", users, &messages_room_options),
+		gen.FieldRelate(field.HasOne, "User", users, &messages_user_options))
+
+	g.ApplyBasic(users, rooms, messages, room_members)
 
 	g.Execute()
 }
